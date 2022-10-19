6e51303d0f00dbcb55e4a1553da2b7483c0b5b2f
emre <emrepoyrazim@gmail.com>
2022-10-19 17:39:10 +0300

    the settings of the car are set to95MHZ

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,5 +9,11 @@ public class Main {
         Auddi.engineCapac="V8";
         Auddi.headlights="blue";
         Auddi.regNo="tuzik";
+
+        //LETS WRİTE OUR OWN CODE
+
+        System.out.println("welcome to my car");
+        System.out.println("the radio is set to 80MHZ");
+        System.out.println("Turn on Air conditoner");
     }
 }
